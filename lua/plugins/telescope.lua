3ead7e1ca3f956675e2c400095e26e5db5e3ff72
Francisco Rojas <francisco.alexis.rojas@gmail.com>
2025-12-16 21:24:30 -0800

    Adds nvim-web-devicons as dependency to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,10 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = 'v0.2.0',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons',
+    },
     config = function() 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files.' })
